b7891f0bc24ddb0d3660f2d654e678ce889655f5
Eesh Tyagi <eesh.t@cleartrip.com>
2018-02-25 21:42:36 +0530

    First draft of personal website

--- src/App.js
+++ src/App.js
@@ -7,14 +7,44 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello, I am your personal website!</h2>
+        <div id="mainContainer">
+        <div id="mainContentBlock">
+          <div id="mainContent">
+            <div id="avatarImageBlock">
+            </div>
+            <div id="introBox">
+              <h1>Eesh</h1>
+              <h2>Web developer</h2>
+              <ul id="linkBox">
+                <li className>
+                  <a href="https://medium.com/@eesh.t" title="Medium">
+                    <i className="fa fa-medium fa-fw link-icon" /><span className="description">Medium</span>
+                  </a>
+                </li>
+                <li className>
+                  <a href="https://www.linkedin.com/in/eesh-tyagi-4905a824/" title="Linkedin">
+                    <i className="fa fa-linkedin fa-fw link-icon" /><span className="description">Linkedin</span>
+                  </a>
+                </li>
+                <li className>
+                  <a href="https://github.com/eeshdarthvader" title="GitHub">
+                    <i className="fa fa-github fa-fw link-icon" /><span className="description">GitHub</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div id="fullBioBox">
+              <p >Takes care of UI UX of different products and innovating usage of upcoming front end technologies.</p>
+            </div>
+          </div>
         </div>
-        <portfolioCardComponent />
-        <p className="App-intro">
-
-        </p>
+        <footer>
+          <div className="footer">
+            <p>© Eesh.
+            </p>
+          </div>
+        </footer>
+      </div>
       </div>
     );
   }
