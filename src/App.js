4532650e6bbc87a51bf01ae7be86e8aabbadf000
Eesh Tyagi <eesh.t@cleartrip.com>
2018-04-11 19:21:55 +0530

    Added medium post

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React, {Component} from 'react'
-import Card from 'react-cards'
-import logo from './logo.svg'
-import './App.css'
+import React, {Component} from 'react';
+import axios from 'axios';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from 'reactstrap';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
+  state = {
+    blog: '',
+  };
+  // BlogPost.js
+  componentDidMount() {
+    axios
+      .get(`https://medium.com/@eesh.t/latest`, {Accept: 'application / json'})
+      //.then(response => this.setState({blog: response.data}))
+      .then(res => {
+        console.log(res.data);
+      });
+  }
   render() {
     return (
       <div className="App">
@@ -44,16 +65,53 @@ class App extends Component {
                   of upcoming front end technologies.
                 </p>
               </div>
+              <div id="mediumBlog">
+                <div id="blogCard">
+                  <Card>
+                    <CardImg
+                      top
+                      width="100%"
+                      src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                      alt="Card image cap"
+                    />
+                    <CardBody>
+                      <CardTitle>Card title</CardTitle>
+                      <CardSubtitle>Card subtitle</CardSubtitle>
+                      <CardText>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </CardText>
+                      <Button outline color="success" size="sm">
+                        Go to post
+                      </Button>
+                    </CardBody>
+                  </Card>
+                </div>
+                <div id="blogCard">
+                  <Card>
+                    <CardImg
+                      top
+                      width="100%"
+                      src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                      alt="Card image cap"
+                    />
+                    <CardBody>
+                      <CardTitle>Card title</CardTitle>
+                      <CardSubtitle>Card subtitle</CardSubtitle>
+                      <CardText>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </CardText>
+                      <Button outline color="success" size="sm">
+                        Go to post
+                      </Button>
+                    </CardBody>
+                  </Card>
+                </div>
+              </div>
             </div>
           </div>
-          <Card
-            src="path/to/img"
-            titleSmallWord="Small word"
-            titleBigWord="Big word"
-            color="Overlay color"
-          >
-            Whatever text Here
-          </Card>
+
           <footer>
             <div className="footer">
               <p>© Eesh.</p>
@@ -61,8 +119,8 @@ class App extends Component {
           </footer>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import 'font-awesome/css/font-awesome.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
