9256551dd216f0cecc1e74e06f2a4cd6d7a2e5ba
eeshtyagi <eesh.tyagi@gmail.com>
2019-01-31 19:52:41 +0530

    Favicon and site title

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,8 +5,8 @@ export default class hero extends Component {
   render() {
     return (
       <div>
-        <h1 className="fw-600">Hello, I am Eesh Tyagi.</h1>
-        <h3>Web Developer</h3>
+        <h1 className="fw-600">Eesh Tyagi</h1>
+        <h3>Lead Web Developer @Cleartrip</h3>
         <p>
           Takes care of UI UX of different products and innovating usage of
           upcoming front end technologies.
