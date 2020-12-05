1058e4cca0880b9a10b0900521e31ee27f815b6b
Eesh Tyagi <eesh.tyagi@gmail.com>
2020-12-05 09:30:17 +0000

    target

--- src/components/header.js
+++ src/components/header.js
@@ -42,7 +42,7 @@ const Header = ({ siteTitle }) => (
                 <a
                   className="c-grey fw-600 mr-5 f-20"
                   href="https://github.com/eeshdarthvader"
-                  target="_blank"
+                  target="_blank" rel="noopener noreferrer"
                 >
                   Github
                 </a>
